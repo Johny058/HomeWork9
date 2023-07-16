68bf5808898f3d99a122216b9235200594fe48c6
Тимур <tuhvatullin_163@mail.ru>
2023-07-16 18:27:51 +0300

    Изменил ДЗ

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         task3();
         task4();
 
+
     }
 
     public static int[] generateRandomArray() {
@@ -17,6 +18,7 @@ public class Main {
         }
         return arr;
     }
+
     public static void task1() {
         System.out.println("Задача 1");
         int arr[] = generateRandomArray();
@@ -24,57 +26,43 @@ public class Main {
         for (int element : arr) {
             sum += element;
         }
-            System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
 
     }
-    public static void task2() {
-        System.out.println("Задача 2");
-        int arr[] = generateRandomArray();
-        int maxArr = -1;
-        int minArr = 200_000;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > maxArr) {
-                maxArr = arr[i];
+            public static void task2 () {
+            System.out.println("Задача 2");
+            int arr[] = generateRandomArray();
+            int maxArr = -1;
+            int minArr = 200_000;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] > maxArr) {
+                    maxArr = arr[i];
+                } else if (arr[i] < minArr) {
+                    minArr = arr[i];
+                }
             }
-        }
-        for (int q = 0; q < arr.length; q++) {
-            if (arr[q] < minArr) {
-                minArr = arr[q];
-            }
-        }
-
-        System.out.println("Минимальная сумма трат за день составила " + minArr + " рублей. " +
-                "Максимальная сумма трат за день составила " + maxArr + " рублей");
-
+            System.out.println("Минимальная сумма трат за день составила " + minArr + " рублей. " +
+                    "Максимальная сумма трат за день составила " + maxArr + " рублей");
     }
-
-    public static void task3() {
+        public static void task3 () {
         System.out.println("Задача 3");
-                int arr[] = generateRandomArray();
+        int arr[] = generateRandomArray();
         float sum = 0;
-        int month = 30;
+        int month = arr.length;
         for (float element : arr) {
-            sum += element / month;
+            sum += element;
         }
-        System.out.println("Средняя сумма трат за месяц составила " + sum + " рублей");
+        System.out.println("Средняя сумма трат за месяц составила " + sum / month + " рублей");
 
     }
-    public static void task4() {
-        System.out.println("Задача 4");
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        reverseFullName[3] = 'n';
-        reverseFullName[2] = 'a';
-        reverseFullName[1] = 'v';
-        reverseFullName[0] = 'I';
-        reverseFullName[4] = ' ';
-        reverseFullName[10] = 'v';
-        reverseFullName[9] = 'o';
-        reverseFullName[8] = 'n';
-        reverseFullName[7] = 'a';
-        reverseFullName[6] = 'v';
-        reverseFullName[5] = 'I';
-                System.out.println(reverseFullName);
+        public static void task4 () {
+            System.out.println("Задача 4");
+            char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+            for( int i = reverseFullName.length -1; i >= 0; i--) {
+                System.out.print(reverseFullName[i]);
+            }
+        }
+    }
 
-    }
-    }
+
 
